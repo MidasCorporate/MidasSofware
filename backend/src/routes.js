de5b680ee1a23312ec4ad20b71fc16bc57127553
alessandro-silva <alessandro.rds03@gmail.com>
2020-05-17 14:17:23 -0400

    add store file

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
+routes.post('/files', upload.single('file'), Filecontroller.store);
 
 export default routes;
