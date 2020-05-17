25b8d0798279124c81d04970996cb3769976b2d1
BRL4528 <blgc.sgo@hotmail.com>
2020-05-17 16:06:56 -0400

    create middlewares users and update user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,11 +6,17 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
