daa941f098492efe1b234f678bc96c2c1af2d0be
BRL4528 <blgc.sgo@hotmail.com>
2020-05-18 16:32:11 -0400

    add Routes.js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,27 +1,38 @@
 module.exports = {
   env: {
-    es6: true,
-    jest: true,
-    browser: true
+      browser: true,
+      es6: true
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: [
+      'prettier/react',
+      'airbnb',
+      'prettier'
+
+  ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    __DEV__: true
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly"
   },
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: "module"
+      ecmaFeatures: {
+          jsx: true
+      },
+      ecmaVersion: 2018,
+      sourceType: "module"
   },
-  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
+  plugins: [
+      "react",
+      "prettier"
+  ],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
-    "import/prefer-default-export": "off",
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "react/jsx-one-expression-per-line": "off",
     "global-require": "off",
@@ -30,8 +41,8 @@ module.exports = {
     "no-underscore-dangle": "off",
     camelcase: "off",
     "no-console": ["error", { allow: ["tron"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    // "react-hooks/rules-of-hooks": "error",
+    // "react-hooks/exhaustive-deps": "warn"
   },
   settings: {
     "import/resolver": {
@@ -39,5 +50,5 @@ module.exports = {
         rootPathSuffix: "src"
       },
     },
-  },
-};
+  }
+ }
